ebb40fd8d23d9785ee82ad9fc610bb18a0fda13a
ARGHYA KAMAL SEN <arghyakamal200@gmail.com>
2025-08-24 18:12:53 +0530

    pdates appcomponent and add pipes in my dyamic-filter-list project

--- 7. dynamic-filter-list/src/app/app.component.ts
+++ 7. dynamic-filter-list/src/app/app.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { FilterPipe } from './filter.pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule, FilterPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'dynamic-filter-list';
+  searchText: string = '';
+  items: string[] = [
+    'Angular Tutorial',
+    'React Tutorial',
+    'Vue.js Tutorial',
+    'JavaScript Basics',
+    'TypeScript Fundamentals',
+    'Building with HTML & CSS',
+    'Introduction to Node.js',
+    'Getting Started with MongoDB',
+    'Web Development with Node.js',
+    'Advanced JavaScript Concepts',
+    'Mastering Angular',
+    'Learning CSS Grid',
+    'Node.js for Beginners',
+    'The Complete JavaScript Guide',
+    'CSS Flexbox in Depth',
+    'Getting Started with Express.js',
+    'Deep Dive into GraphQL',
+    'Modern Web Development Trends',
+    'Building REST APIs with Express',
+    'Introduction to Git and GitHub',
+    'Web Accessibility Essentials',
+  ];
 }
